a7ab8b3836add7c58fddec4334ea3e1ab05ad0cd
Mychal Thompson <mychalrthompson@gmail.com>
2019-03-28 21:51:09 -0400

    Adds missing reflect polyfill and works around AoT bug in Angular v7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageModule } from './pages/home/home.page.module';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => HomePageModule }
+  { path: '', loadChildren: './pages/home/home.page.module#HomePageModule' }
 ];
 
 @NgModule({
